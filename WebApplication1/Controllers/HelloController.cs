d4b13b65d8e2affba64098ebd5f7f3ce90795575
Stella <l9zhang@ucsd.edu>
2023-11-02 22:35:09 -0700

    Add project files.

---
+++ WebApplication1/Controllers/HelloController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Text.Json.Nodes;
+
+namespace WebApplication1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HelloController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("Hello World!");
+        }
+        [HttpPost]
+        public IActionResult Post(JsonObject payload)
+        {
+            //can be exposed some attack because we didn't validate the payload
+            return Ok(payload);
+        }
+    }
+}
